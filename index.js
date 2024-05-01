6206f4d45405c9c662f71ecb03bd941c133a1254
Alien <farhan10670@gmail.com>
2024-05-02 00:49:33 +0600

    commit after 2nd cls

---
+++ index.js
@@ -0,0 +1,20 @@
+var Num = 20;
+console.log(typeof(Num));
+
+num = toString(Num);
+console.log(typeof(Num)) ;
+
+var n = "20" ;
+n = parseInt(n) ;
+console.log(typeof(n));
+
+
+var x = 2.5641
+console.log(x.toFixed(2)); // after d decimal point ,return string 
+
+console.log(x.toPrecision(3)) //totol number ,return string
+
+console.log(typeof(Number("21"))); 
+
+console.log(Number(true));
+console.log(Number(false));
